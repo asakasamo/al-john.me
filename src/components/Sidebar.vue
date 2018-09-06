7ed472028010d1d286ca9c5f6406c9fc57ea1476
asakasamo <a.sakasamo@gmail.com>
2018-09-06 04:06:43 -0500

    Scaffold the full site

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,6 @@
+<template>
+   <h2>Sidebar</h2>
+</template>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import Sidebar from "./components/Sidebar.vue";
+
+Vue.config.productionTip = false;
+
+Vue.component("sidebar", Sidebar);
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+   router,
+   render: (h) => h(App)
+}).$mount("#app");
