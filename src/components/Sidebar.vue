10ab923f0eab2fdabbf6c6ff28fc3c7b1c0e5801
asakasamo <a.sakasamo@gmail.com>
2018-09-06 04:34:58 -0500

    Add unstyled content to the pages

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,35 @@
 <template>
-   <h2>Sidebar</h2>
+   <div id="sidebar">
+      <div id="logo">
+         <h3>Al-John Sakasamo</h3>
+         <h5>Software Engineer</h5>
+      </div>
+
+      <nav>
+         <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/my-work">My Work</router-link></li>
+            <li><router-link to="/about">About Me</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li>
+         </ul>
+      </nav>
+
+      <div class="socs">
+         <a href="https://www.linkedin.com/in/asakasamo/" class="social">
+            <div class="lin"></div>
+            <span>asakasamo</span>
+         </a>
+         <a href="https://github.com/asakasamo" class="social">
+            <div class="git"></div>
+            <span>asakasamo</span>
+         </a>
+      </div>
+   </div>
 </template>
 
 <style scoped lang="scss">
+#sidebar {
+   width: 250px;
+   flex-basis: 250px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-   <div class="home">
-      Home Page
+   <div id="home">
+      <h1>Hey, I'm a burgeoning
+         <br>
+         <span class="highlight">Software Engineer!</span>
+      </h1>
+
+      <h4>Check out my 
+         <a href="#">skillset</a>, 
+         <a href="#">projects</a>, 
+         <a href="#">pdf resume</a>, 
+         or just navigate using the menu.
+      </h4>
+
+      <h4 class="mt-5">- Al-John Sakasamo</h4>
    </div>
 </template>
 
